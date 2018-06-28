fd5988db9702eb0b527f53a10c5c6e5df7e69f14
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2018-06-28 23:56:52 +0200

    x

--- dlga/scripts/app.js
+++ dlga/scripts/app.js
@@ -9,8 +9,9 @@
   }
 
   if ('serviceWorker' in navigator) {
+    console.log("sadasdasd")
     navigator.serviceWorker
-             .register('./service-worker.js')
+             .register('service-worker.js')
              .then(function() { console.log('Service Worker Registered'); });
   }
 })();
