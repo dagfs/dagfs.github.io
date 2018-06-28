96aae0067d2b713f3e640b4f9678b41d8dc5d551
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2018-06-29 00:39:45 +0200

    x

--- dlga/service-worker.js
+++ dlga/service-worker.js
@@ -1,7 +1,3 @@
-
-
-var dataCacheName = 'dlga-v1.2';
-var cacheName = 'dlga-v1.2';
 var filesToCache = [
   './',
   './index.html',
@@ -10,37 +6,41 @@ var filesToCache = [
   './styles/inline.css',
 ];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
+var staticCacheName = 'pages-cache-v1';
+
+self.addEventListener('install', function(event) {
+  console.log('Attempting to install service worker and cache static assets');
+  event.waitUntil(
+    caches.open(staticCacheName)
+    .then(function(cache) {
       return cache.addAll(filesToCache);
     })
   );
 });
 
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(r) {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then(function(response) {
+                return caches.open(staticCacheName).then(function(cache) {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
+          return Promise.all(keyList.map(function(key) {
+        if(cacheName.indexOf(key) === -1) {
           return caches.delete(key);
         }
       }));
     })
   );
-  /*
-   * Fixes a corner case in which the app wasn't returning the latest data.
-   * You can reproduce the corner case by commenting out the line below and
-   * then doing the following steps: 1) load app for first time so that the
-   * initial New York City data is shown 2) press the refresh button on the
-   * app 3) go offline 4) reload the app. You expect to see the newer NYC
-   * data, but you actually see the initial data. This happens because the
-   * service worker is not yet activated. The code below essentially lets
-   * you activate the service worker faster.
-   */
-  return self.clients.claim();
 });
---
+++ dlga/todo.md
@@ -0,0 +1,1 @@
+https://developers.google.com/web/fundamentals/app-install-banners/
