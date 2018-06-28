038beac91f53f040ad042f8ecde88b124ebd8954
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2018-06-28 23:09:59 +0200

    dlga init

---
+++ dlga/scripts/app.js
@@ -0,0 +1,16 @@
+
+
+
+(function() {
+  'use strict';
+  var main = document.getElementsByTagName("main")[0];
+  for(var i = 0; i < data.length; i++){
+    main.innerHTML += "<details><summary>" + data[i].title+"</summary><p>"+data[i].text+"</p></details>";
+  }
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+})();
---
+++ dlga/service-worker.js
@@ -0,0 +1,34 @@
+var dataCacheName = 'dlga-v1.1';
+var cacheName = 'dlga-v1.1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/scripts/data.json',
+  '/styles/inline.css',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
